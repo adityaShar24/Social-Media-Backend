b755c7c8149883e6ca2d3eb062ef82809ecc169b
adityaShar24 <adityarajansharma1234@gmail.com>
2023-08-24 17:33:00 +0530

    add find by username

--- src/models/user_model.py
+++ src/models/user_model.py
@@ -8,3 +8,7 @@ class User:
     def save_user(self):
         user_id = users_collection.insert_one({'username':self.username , 'password':self.password}).inserted_id
         return user_id
+    
+    def find_by_username(username):
+        user_id = users_collection.find_one({'username':username})
+        return user_id
