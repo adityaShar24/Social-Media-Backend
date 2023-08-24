707d35d1158a72a71f2eff7735f23954ccc0f6a8
adityaShar24 <adityarajansharma1234@gmail.com>
2023-08-24 17:38:50 +0530

    add update utils

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,2 +1,9 @@
 CONNECTED_TO_MONGODB = 'Connection to MongoDB Successful!'
 CONNECTION_FAILED = 'Connection to MongoDB Failed'
+
+HTTP_201_CREATED = 201
+HTTP_400_BAD_REQUEST = 400
+
+USERNAME_MISSING_ERROR = 'username feild should not be empty'
+PASSWORD_MISSING_ERROR = 'password field should not be empty'
+EXISITING_USER_ERROR = 'username already exists, please enter a unique username'
