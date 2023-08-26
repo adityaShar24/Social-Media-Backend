40ff3163562bf2cbd8b4d5d5b4060da6787b3dec
adityaShar24 <adityarajansharma1234@gmail.com>
2023-08-26 08:44:45 +0530

    add update constants

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -9,3 +9,6 @@ PASSWORD_MISSING_ERROR = 'password field should not be empty'
 EXISITING_USERNAME_ERROR = 'username already exists, please enter a unique username'
 
 USER_REGISTERED_MESSAGE = 'user {username} has been registered successfully!'
+USER_NOT_EXISTS_ERROR = 'user {username} does not exists'
+
+REGISTER_USER_ENDPOINT = 'auth_bp.register_user_wrapper'
