9720a1db9966ab67ec5a7408165705286484b47a
adityaShar24 <adityarajansharma1234@gmail.com>
2023-08-26 08:49:14 +0530

    add blueprint and middleware

--- src/app.py
+++ src/app.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from routes.user_router import auth_bp
+from middlewares.user_middleware import register_user_middleware
 
 app = Flask(__name__)
 
-
 app['config'] = "SECRET_KEY"
+
+app.before_request(register_user_middleware)
+app.register_blueprint(auth_bp)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0' , debug=True)
