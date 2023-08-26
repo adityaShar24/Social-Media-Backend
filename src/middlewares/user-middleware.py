290a1979b55003a9fff035616af61b381d568e3b
adityaShar24 <adityarajansharma1234@gmail.com>
2023-08-26 08:44:12 +0530

    add register request endpoint

--- src/middlewares/user-middleware.py
+++ src/middlewares/user-middleware.py
@@ -1,25 +1,26 @@
 from flask import request , make_response , json
-from utils.constants import USERNAME_MISSING_ERROR , HTTP_400_BAD_REQUEST , PASSWORD_MISSING_ERROR , EXISITING_USERNAME_ERROR , USER_NOT_EXISTS_ERROR
+from utils.constants import USERNAME_MISSING_ERROR , HTTP_400_BAD_REQUEST , PASSWORD_MISSING_ERROR , EXISITING_USERNAME_ERROR , USER_NOT_EXISTS_ERROR , REGISTER_USER_ENDPOINT
 from models.user_model import User
 
 
 def register_user_middleware():
-    body = json.loads(request.data)
-    username = body['username']
-    password = body['password']
-    
-    if not username:
-        return make_response({'message': USERNAME_MISSING_ERROR} , HTTP_400_BAD_REQUEST)
-    
-    if not password:
-        return make_response({'message':PASSWORD_MISSING_ERROR } , HTTP_400_BAD_REQUEST)
-    
-    user = User(username , password)
-    
-    existing_user = user.find_by_username(username)
-    
-    if existing_user:
-        return make_response({'message':EXISITING_USERNAME_ERROR} , HTTP_400_BAD_REQUEST)
+    if request.endpoint == REGISTER_USER_ENDPOINT:
+        body = json.loads(request.data)
+        username = body['username']
+        password = body['password']
+        
+        if not username:
+            return make_response({'message': USERNAME_MISSING_ERROR} , HTTP_400_BAD_REQUEST)
+        
+        if not password:
+            return make_response({'message':PASSWORD_MISSING_ERROR } , HTTP_400_BAD_REQUEST)
+        
+        user = User(username , password)
+        
+        existing_user = user.find_by_username(username)
+        
+        if existing_user:
+            return make_response({'message':EXISITING_USERNAME_ERROR} , HTTP_400_BAD_REQUEST)
     
     
 def login_user_middleware():
