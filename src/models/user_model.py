3a0933634bf929e8e51d174489cc9e9b60eec633
adityaShar24 <adityarajansharma1234@gmail.com>
2023-08-28 13:34:16 +0530

    add parameters

--- src/models/user_model.py
+++ src/models/user_model.py
@@ -4,11 +4,15 @@ class User:
     def __init__(self , username , password):
         self.username = username
         self.password = password
+        self.request_sent = []
+        self.request_received = []
+        self.friends = []
     
     def save_user(self):
-        user_id = users_collection.insert_one({'username':self.username , 'password':self.password}).inserted_id
+        user_id = users_collection.insert_one({'username':self.username , 'password':self.password , 'friends': self.friends , 'request_sent': self.request_sent , 'request_received':self.request_received}).inserted_id
         return user_id
     
     def find_by_username(username):
         user = users_collection.find_one({'username':username})
         return user
+    
