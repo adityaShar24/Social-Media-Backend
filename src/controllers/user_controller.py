f6c7eb5baa095cc0b25e659f10703e1e64b13e65
adityaShar24 <adityarajansharma1234@gmail.com>
2023-08-28 13:56:19 +0530

    add make request

--- src/controllers/user_controller.py
+++ src/controllers/user_controller.py
@@ -1,7 +1,8 @@
 from flask import request , json , make_response
 from models.user_model import User
+from models.make_request_model import Request
 from flask_jwt_extended import create_access_token
-from utils.constants import HTTP_201_CREATED , HTTP_400_BAD_REQUEST , USER_REGISTERED_MESSAGE , INVALID_PASSWORD_ERROR
+from utils.constants import HTTP_201_CREATED , HTTP_400_BAD_REQUEST , USER_REGISTERED_MESSAGE , INVALID_PASSWORD_ERROR , REQUEST_SENT_MESSAGE
 import bson.json_util as json_util
 import datetime
 
@@ -18,12 +19,20 @@ def login():
     body = json.loads(request.data)
     username = body['username']
     password = body['password']
-    user = User(username , password)
-    
-    if password != user[password]:
+    user = User.find_by_username(username)
+        
+    if password != user['password']:
         return make_response({'message':INVALID_PASSWORD_ERROR} , HTTP_400_BAD_REQUEST)
     
-    access_token = create_access_token(identity= username , fresh= datetime.timedelta(minutes=30))
-    
-    return make_response({'message':{'access_token': access_token }} , HTTP_201_CREATED)       
+    access_token = create_access_token(identity=username , fresh=datetime.timedelta(minutes=30))
+    return make_response({'message':{'access token':access_token}} , HTTP_201_CREATED)
 
+
+def make_request():
+    body = json.loads(request.data)
+    From = body['userID']
+    to = body['userID']
+    
+    request = Request.make_request(From , to)
+    json_version = json_util.dumps(request)
+    return make_request({'message': REQUEST_SENT_MESSAGE , 'request': json_version} , HTTP_201_CREATED)
