b44d2b69d285358d16d2d9927bffdd8ea15576d7
adityaShar24 <adityarajansharma1234@gmail.com>
2023-08-29 11:54:23 +0530

    update constants

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -13,6 +13,7 @@ USER_NOT_EXISTS_ERROR = 'user {username} does not exists'
 
 REGISTER_USER_ENDPOINT = 'auth_bp.register_user_wrapper'
 LOGIN_USER_ENDPOINT = 'auth_bp.login_user_wrapper'
+MAKE_REQUEST_ENDPOINT = 'auth_bp.make_request_wrapper'
 
 INVALID_PASSWORD_ERROR = 'Invalid password , please provide the correct one.'
 USER_ID_MISSING_ERROR = 'userId field should not be empty'
