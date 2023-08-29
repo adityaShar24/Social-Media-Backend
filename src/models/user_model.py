8b926970e1b4273ec2b671221d76d06303bf7610
adityaShar24 <adityarajansharma1234@gmail.com>
2023-08-29 11:52:50 +0530

    add request id

--- src/models/user_model.py
+++ src/models/user_model.py
@@ -8,6 +8,12 @@ class User:
         self.request_received = []
         self.friends = []
     
+    def add_request_id(self , From , to):
+        self.request_sent.append(From)
+        self.request_received.append(to)
+        self.save_user()
+        return self.request_sent and self.request_received
+    
     def save_user(self):
         user_id = users_collection.insert_one({'username':self.username , 'password':self.password , 'friends': self.friends , 'request_sent': self.request_sent , 'request_received':self.request_received}).inserted_id
         return user_id
