19dc2aba359d0fc3b6f7e420b2eacdd87deebe22
adityaShar24 <adityarajansharma1234@gmail.com>
2023-09-04 15:53:19 +0530

    add remove request route

--- src/routes/user_router.py
+++ src/routes/user_router.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from controllers.user_controller import register , login , make_request
+from controllers.user_controller import register , login , make_request , remove_request
 auth_bp = Blueprint('auth_bp' , __name__ )
 
 
@@ -13,4 +13,8 @@ def login_user_wrapper():
 
 @auth_bp.post('/make-request')
 def make_request_wrapper():
-    return make_request
+    return make_request()
+
+@auth_bp.delete('/remove_request')
+def remove_request_wrapper():
+    return remove_request()
