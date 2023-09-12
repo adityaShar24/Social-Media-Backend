7e0dcb19998a99bddeb53fc802d32ab42abb06eb
adityaShar24 <adityarajansharma1234@gmail.com>
2023-09-12 16:52:11 +0530

    add add posts

--- src/models/posts_model.py
+++ src/models/posts_model.py
@@ -1,4 +1,4 @@
-from database.mongo import posts_collection
+from database.mongo import posts_collection , users_collection
 from bson.objectid import ObjectId
 class Posts:
     def __init__(self, url, caption, userId , likes=0):
@@ -9,5 +9,13 @@ class Posts:
         self.comments = []
 
     def save_posts(self):
-        saved_posts = posts_collection.insert_one({'url':self.url , 'likes':self.likes , 'caption':self.caption , 'userID':self.userId , 'comments':self.comments})
+        saved_posts = posts_collection.insert_one({'url':self.url , 'likes':self.likes , 'caption':self.caption , 'userId':self.userId , 'comments':self.comments})
         return saved_posts
+    
+    def add_posts(postId , userId):
+        post = users_collection.update_one({"_id": ObjectId(userId)}, { "$push": { "posts": postId}})
+        return post
+        
+        
+        
+        
