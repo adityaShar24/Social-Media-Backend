d625683b8da6e3abd32c71aad4f2d422a4357fdb
adityaShar24 <adityarajansharma1234@gmail.com>
2023-09-13 18:22:15 +0530

    Added add-postId route

--- src/routes/post_router.py
+++ src/routes/post_router.py
@@ -1,8 +1,12 @@
 from flask import Blueprint
-from controllers.post_controller import post
+from controllers.post_controller import post , add_postId
 
 post_bp = Blueprint('post_bp' , __name__)
 
 @post_bp.post('/post')
 def post_wrapper():
     return post()
+
+@post_bp.post('/add-postId')
+def add_postId_wrapper():
+    return add_postId()
