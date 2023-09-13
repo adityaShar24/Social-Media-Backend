c6a65bfc3901955f8f39c32fc49540b13cf12cc6
adityaShar24 <adityarajansharma1234@gmail.com>
2023-09-13 18:21:10 +0530

    Added add postId

--- src/controllers/post_controller.py
+++ src/controllers/post_controller.py
@@ -2,6 +2,7 @@ from flask import request , json , make_response
 from models.posts_model import Posts
 from utils.constants import HTTP_201_CREATED , POST_UPLOADED_MESSAGE
 import bson.json_util as json_util
+from bson.objectid import ObjectId as Ob
 
 
 def post():
@@ -15,6 +16,19 @@ def post():
 
     saved_posts = posts.save_posts()
     
-    json_version = json_util.dumps(saved_posts)
+    # json_version = json_util.dumps(saved_posts)
     
-    return make_response({'message': POST_UPLOADED_MESSAGE, "post": json_version} , HTTP_201_CREATED)
+    return make_response({'message': POST_UPLOADED_MESSAGE} , HTTP_201_CREATED)
+
+
+def add_postId():
+    body = json.loads(request.data)
+    
+    userId = body['userId']
+    
+    postId = body['postId']
+    
+    post = Posts.add_posts(Ob(userId) , Ob(postId))
+    print(post)
+    
+    return make_response({'message': "added postId in users posts!"} , HTTP_201_CREATED)
