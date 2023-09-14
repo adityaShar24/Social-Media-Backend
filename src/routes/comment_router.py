c8304a46264310a907e56e0b10ba25eeaeb42924
adityaShar24 <adityarajansharma1234@gmail.com>
2023-09-14 20:20:48 +0530

    Added add commentId router

--- src/routes/comment_router.py
+++ src/routes/comment_router.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from controllers.comment_controller import comment
+from controllers.comment_controller import comment , add_commentId
 
 
 comment_bp = Blueprint('comment_bp' , __name__)
@@ -7,3 +7,7 @@ comment_bp = Blueprint('comment_bp' , __name__)
 @comment_bp.post('/comment')
 def comment_wrapper():
     return comment()
+
+@comment_bp.post('/add-commentId')
+def add_commmenId_wrapper():
+    return add_commentId()
