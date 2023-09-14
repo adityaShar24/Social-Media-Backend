d40cd3d01a830bef44f7fc1f4cdf21620b413b5e
adityaShar24 <adityarajansharma1234@gmail.com>
2023-09-14 20:17:16 +0530

    Added add commentId

--- src/controllers/comment_controller.py
+++ src/controllers/comment_controller.py
@@ -1,5 +1,5 @@
 from flask import request , json , make_response
-from utils.constants import HTTP_201_CREATED , COMMENT_POSTED_MESSAGE
+from utils.constants import HTTP_201_CREATED , COMMENT_POSTED_MESSAGE , ADDED_COMMENTID_MESSAGE
 from models.comment_model import Comment
 import bson.json_util as bson
 
@@ -18,3 +18,14 @@ def comment():
     json_saved_comments = bson.dumps(saved_comments)
     
     return make_response({'message': COMMENT_POSTED_MESSAGE, 'comment': json_saved_comments} , HTTP_201_CREATED)
+
+
+def add_commentId():
+    body = json.loads(request.data)
+    
+    userId = body['userId']
+    commentId = body['commentId']
+    
+    Comment.add_commentId(userId , commentId)
+    
+    return make_response({'message' : ADDED_COMMENTID_MESSAGE } , HTTP_201_CREATED)
