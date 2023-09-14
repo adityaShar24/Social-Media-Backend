34eb9017a62de11acd35a45ce8e3cd50d0771fb0
adityaShar24 <adityarajansharma1234@gmail.com>
2023-09-14 20:07:06 +0530

    Added reply comment middleware

--- src/middlewares/comment_middleware.py
+++ src/middlewares/comment_middleware.py
@@ -1,7 +1,8 @@
 from flask import request, json , make_response
-from utils.constants import COMMENT_REQUIRED , COMMENT_ENDPOINT , HTTP_400_BAD_REQUEST , POSTID_MISSING_ERROR , USER_ID_MISSING_ERROR , PARENT_COMMENTID_MISSING_ERROR
+from utils.constants import COMMENT_REQUIRED , COMMENT_ENDPOINT , HTTP_400_BAD_REQUEST , POSTID_MISSING_ERROR , USER_ID_MISSING_ERROR , COMMENTID_MISSING_ERROR , REPLY_COMMENT_ENDPOINT
 
-def comment_middleware(request):
+
+def comment_middleware():
     if request.endpoint == COMMENT_ENDPOINT:
         body = json.loads(request.data) 
         comment = body['comment']
@@ -18,5 +19,16 @@ def comment_middleware(request):
         if not userId: 
             return make_response({'message' : USER_ID_MISSING_ERROR} , HTTP_400_BAD_REQUEST)
         
-        if not parent_commentId:
-            return make_response({'message' : PARENT_COMMENTID_MISSING_ERROR} , HTTP_400_BAD_REQUEST)
+def reply_comment_middleware():
+    if request.endpoint == REPLY_COMMENT_ENDPOINT:
+        body = json.loads(request.data)
+        
+        userId = body['userId']
+        commentId = body['commentId']
+        
+        if not userId:
+            return make_response({'message' : USER_ID_MISSING_ERROR} , HTTP_400_BAD_REQUEST)
+        
+        if not commentId:
+            return make_response({'message' :  COMMENTID_MISSING_ERROR} , HTTP_400_BAD_REQUEST)
+        
