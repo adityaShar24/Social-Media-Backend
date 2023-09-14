13ffbb91bc42b2cf33ffdb509e1d5ebc523d0a69
adityaShar24 <adityarajansharma1234@gmail.com>
2023-09-14 19:13:39 +0530

    Added comment model

---
+++ src/models/comment_model.py
@@ -0,0 +1,17 @@
+from database.mongo import comments_collection
+from bson.objectid import ObjectId as Ob
+
+class Comment:
+    def __init__(self , comment , postId , userId , parent_commentId):
+        self.comment = comment
+        self.postId = postId    
+        self.userId = userId
+        self.parent_commentId = parent_commentId if parent_commentId else None
+        
+    def save_comments(self):
+        comments_collection.insert_one({
+            'comment' : self.comment,
+            'postId' : self.postId,
+            'userId' : self.userId,
+            'parent_commentId' : self.parent_commentId
+        })
