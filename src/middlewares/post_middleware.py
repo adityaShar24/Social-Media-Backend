efb1b720e6f315de02d12c87ca077c8e4e7c84d7
adityaShar24 <adityarajansharma1234@gmail.com>
2023-09-15 16:16:34 +0530

    refactor

--- src/middlewares/post_middleware.py
+++ src/middlewares/post_middleware.py
@@ -2,25 +2,26 @@ from flask import request , json , make_response
 from utils.constants import HTTP_400_BAD_REQUEST , ADD_POST_ENDPOINT , USER_ID_MISSING_ERROR , POSTID_MISSING_ERROR , ADD_POSTID_ENDPOINT
 
 def post_middleware():
-        if request.endpoint == ADD_POST_ENDPOINT:
-            body = json.loads(request.data)
+    if request.endpoint == ADD_POST_ENDPOINT:
+        body = json.loads(request.data)
             
-            url = body['url']
-            caption = body['caption']
-            userId = body['userId']
+        url = body['url']
+        caption = body['caption']
+        userId = body['userId']
             
-            if not url:
-                return make_response({'message':'Post url should not be empty'} , HTTP_400_BAD_REQUEST)
+        if not url:
+            return make_response({'message':'Post url should not be empty'} , HTTP_400_BAD_REQUEST)
             
-            if not caption:
-                return make_response({'message':'Post caption should not be empty'} , HTTP_400_BAD_REQUEST)
+        if not caption:
+            return make_response({'message':'Post caption should not be empty'} , HTTP_400_BAD_REQUEST)
             
-            if not userId:
-                return make_response({'message':"userId cannot be empty"} , HTTP_400_BAD_REQUEST)
+        if not userId:
+            return make_response({'message':"userId cannot be empty"} , HTTP_400_BAD_REQUEST)
             
 
 def add_postId_middleware():
     if request.endpoint == ADD_POSTID_ENDPOINT:
+        print(ADD_POSTID_ENDPOINT)
         body = json.loads(request.data)
         
         userId = body['userId']
@@ -28,7 +29,7 @@ def add_postId_middleware():
         postId = body['postId']
         
         if not userId:
-            make_response({'message': USER_ID_MISSING_ERROR} , HTTP_400_BAD_REQUEST)
+            return make_response({'message': USER_ID_MISSING_ERROR} , HTTP_400_BAD_REQUEST)
             
         if not postId:
-            make_response({'message': POSTID_MISSING_ERROR} , HTTP_400_BAD_REQUEST)
+            return make_response({'message': POSTID_MISSING_ERROR} , HTTP_400_BAD_REQUEST)
