31c55df6b4a4ce1932d5ef761f42fbdec18c0956
adityaShar24 <adityarajansharma1234@gmail.com>
2023-09-21 15:23:10 +0530

    Modified comment model

--- src/database/models/posts_model.py
+++ src/database/models/posts_model.py
@@ -1,14 +1,26 @@
-from database.mongo import posts_collection , users_collection
-from bson.objectid import ObjectId
-class Posts:
-    def __init__(self, url, caption, userId , likes=0):
-        self.url = url
-        self.likes = likes
-        self.caption = caption
-        self.userId = ObjectId(userId)
-        self.comments = []
-
-    def save_posts(self):
-        saved_posts = posts_collection.insert_one({'url':self.url , 'likes':self.likes , 'caption':self.caption , 'userId':self.userId, 'comments':self.comments}).inserted_id
-        users_collection.update_one({"_id": self.userId }, { "$push": { "posts": ObjectId(saved_posts) } })
-        return saved_posts
+schema = {
+    'bsonType': 'object',
+    'required': ['url', 'caption', 'userId'],
+    'properties': {
+        'url': {
+            'bsonType': 'string',
+            'description': 'must be a string and is required'
+        },
+        'caption': {
+            'bsonType': 'string',
+            'description': 'must be a string and is required'
+        },
+        'userId': {
+            'bsonType': 'objectId',
+            'description': 'must be a string and is required'
+        },
+        'likes': {
+            'bsonType': 'int',
+            'description': 'must be a string and is required'
+        },
+        'comments': {
+            'bsonType': 'array',
+            'description': 'must be a string and is required'
+        }
+    }
+}
