20998346f645ce564beaf494fbf668404a4aa7f2
adityaShar24 <adityarajansharma1234@gmail.com>
2023-09-21 15:20:23 +0530

    Modified comment model

--- src/database/models/comment_model.py
+++ src/database/models/comment_model.py
@@ -1,23 +1,22 @@
-from database.mongo import comments_collection, posts_collection
-from bson.objectid import ObjectId as Ob
-
-class Comment:
-    def __init__(self , comment , postId , userId , parent_commentId):
-        self.comment = comment
-        self.postId = postId    
-        self.userId = userId
-        self.parent_commentId = parent_commentId if parent_commentId else None
-        
-    def save_comments(self):
-        commentID = comments_collection.insert_one({
-            'comment' : self.comment,
-            'postId' : self.postId,
-            'userId' : self.userId,
-            'parent_commentId' : self.parent_commentId
-        }).inserted_id
-        return commentID
-
-        
-    def add_commentId(postId , commentId):
-        posts_collection.update_one({'_id' : Ob(postId)} , {'$push' : {'comments' : Ob(commentId) } } )
-    
+schema = {
+    'bsonType': 'object',
+    'required': ['comment', 'postId', 'userId'],
+    'properties': {
+        'comment': {
+            'bsonType': 'string',
+            'description': 'must be a string and is required'
+        },
+        'postId': {
+            'bsonType': 'objectId',
+            'description': 'must be a string and is required'
+        },
+        'userId': {
+            'bsonType': 'objectId',
+            'description': 'must be a string and is required'
+        },
+        'parent_commentId': {
+            'bsonType': 'objectId',
+            'description': 'must be a string and is required'
+        }
+    }
+}
