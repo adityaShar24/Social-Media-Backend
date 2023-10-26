cd49b4b6f3527a3dc87af0e8be73c85a5c628b05
adityaShar24 <adityarajansharma1234@gmail.com>
2023-10-26 13:42:28 +0530

    Added add member

--- src/controllers/rooms_controller.py
+++ src/controllers/rooms_controller.py
@@ -1,8 +1,8 @@
 from flask import json , request , make_response
 from database.repositories.rooms_repository import RoomsRepository
-from database.repositories.user_repository import UserRepository
 from utils.constants import HTTP_201_CREATED , ROOM_CREATED_MESSAGE , ROOM_DELETED_MESSAGE , ROOM_MEMBER_ADDED_MESSAGE
 import bson.json_util as json_util
+from bson.objectid import ObjectId
 
 def create_room():
     body = json.loads(request.data)
@@ -13,4 +13,19 @@ def create_room():
     
     json_version = json_util.dumps(roomID)
     return make_response({'message': ROOM_CREATED_MESSAGE , "room": json_version} , HTTP_201_CREATED)
+    
+def add_member():
+    body = json.loads(request.data)
+    roomId = body['roomId']
+    userId = body['userId']
+    
+    members = RoomsRepository().update_one({"_id":ObjectId(roomId)} , {"$push": {"members": userId} })
+    
+    json_version = json_util.dumps(members)
+    
+    return make_response({"message": ROOM_MEMBER_ADDED_MESSAGE , "members": json_version} , HTTP_201_CREATED)
+        
+    
+    
+    
     
