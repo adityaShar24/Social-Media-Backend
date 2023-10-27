066171334db45fb1237892b7c4561283d4a679d6
adityaShar24 <adityarajansharma1234@gmail.com>
2023-10-27 14:00:37 +0530

    Added get all  message route

--- src/routes/message_router.py
+++ src/routes/message_router.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
-from controllers.message_controller import send_message , delete_message 
+from controllers.message_controller import send_message , delete_message , get_all_messages
+from flask_jwt_extended import jwt_required
+
 message_bp = Blueprint('message_bp', __name__)
 
 @message_bp.post('/send-message')
@@ -9,4 +11,9 @@ def send_message_wrapper():
 @message_bp.post('/delete-message')
 def delete_message_wrapper():
     return delete_message()
+
+@message_bp.get('/get-all-messages')
+@jwt_required()
+def get_all_messages_wrapper():
+    return get_all_messages()
 
