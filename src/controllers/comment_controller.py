b8c12685b949a213f39e7a23539aea5cd4f37d69
adityaShar24 <adityarajansharma1234@gmail.com>
2023-10-27 20:39:04 +0530

    Refactor

--- src/controllers/comment_controller.py
+++ src/controllers/comment_controller.py
@@ -3,7 +3,7 @@ from utils.constants import HTTP_201_CREATED , COMMENT_POSTED_MESSAGE , ADDED_CO
 from database.repositories.comment_repository import CommentsRepository
 from database.repositories.user_repository import UserRepository
 import bson.json_util as bson
-
+from bson.objectid import ObjectId
 
 def comment():
     body = json.loads(request.data)
@@ -13,11 +13,10 @@ def comment():
     parent_commentId = body['parent_commentId'] if 'parent_commentId' in body else None
     
     
-    saved_comments = CommentsRepository().create({'comment': comment , 'postId': postId , 'userId': userId , 'parent_commentId': parent_commentId})
+    saved_comments = CommentsRepository().create({'comment': comment , 'postId': ObjectId(postId) , 'userId': ObjectId(userId) , 'parent_commentId': ObjectId(parent_commentId)})
     UserRepository().update_one({"_id": userId }, { "$push": { "comments": saved_comments } })
     
     json_saved_comments = bson.dumps(saved_comments)
     
     return make_response({'message': COMMENT_POSTED_MESSAGE, 'comment': json_saved_comments} , HTTP_201_CREATED)
-
 
