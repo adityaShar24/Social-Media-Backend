2766f9c4f4a0fcad934e414680d4a9ee7556de2d
adityaShar24 <adityarajansharma1234@gmail.com>
2023-10-27 13:59:36 +0530

    Added delete message route

--- src/routes/message_router.py
+++ src/routes/message_router.py
@@ -1,9 +1,12 @@
 from flask import Blueprint
-from controllers.message_controller import send_message
-
+from controllers.message_controller import send_message , delete_message 
 message_bp = Blueprint('message_bp', __name__)
 
 @message_bp.post('/send-message')
 def send_message_wrapper():
     return send_message()
+
+@message_bp.post('/delete-message')
+def delete_message_wrapper():
+    return delete_message()
 
