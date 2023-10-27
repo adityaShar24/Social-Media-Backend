43c8d9dc9f7365408e01699e13817a9b26e74320
adityaShar24 <adityarajansharma1234@gmail.com>
2023-10-27 13:31:28 +0530

    Added get all messages

--- src/controllers/message_controller.py
+++ src/controllers/message_controller.py
@@ -3,6 +3,7 @@ import bson.json_util as json_uitl
 from database.repositories.message_repository import MessagesRepository
 from utils.constants import HTTP_201_CREATED , MESSAGE_SENT , MESSAGE_DELETED 
 from flask_socketio import emit
+from bson.objectid import ObjectId
 
 
 def send_message():
@@ -21,8 +22,17 @@ def send_message():
 def delete_message():
     messageId = request.args.get('messageId')
     
-    MessagesRepository().delete_one(messageId)
+    MessagesRepository().delete_one({"_id":ObjectId(messageId)})
     
     return make_response({'message': MESSAGE_DELETED} , HTTP_201_CREATED)
+
+def get_all_messages():
+    roomId = request.args.get('roomId')
+    
+    messages = MessagesRepository().find_one({"roomId":ObjectId(roomId)})
+    list_messages = list(messages)
+    
+    json_version = json_uitl.dumps(list_messages)
+    return make_response({'data':json_version} , HTTP_201_CREATED)
     
     
